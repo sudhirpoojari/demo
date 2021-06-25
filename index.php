b4165d5a973869b7028f68052094634ebff479f8
sudhirpoojari <86469974+sudhirpoojari@users.noreply.github.com>
2021-06-25 15:29:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>  
+
+<html>  
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>SIRD</title> 
+   </head> 
+  
+   <body> 
+      <header role = "banner"> 
+         <h1>Government of Karnataka</h1> 
+         <p>Abdul Nazir Sab State Institute of Rural Development and Panchayat Raj</p> 
+         
+      </header> 
+   
+ 
+   
+      
+      <footer> 
+         <p>Created by <a href = "https://tutorialspoint.com/">Tutorials Point</a></p> 
+      </footer> 
+   
+   </body> 
+</html> 
